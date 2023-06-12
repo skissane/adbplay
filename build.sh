939ff0f724c8ab0df91988aea0e4ce0e6d7f36ad
Simon Kissane <skissane@gmail.com>
2023-06-12 11:29:32 +1000

    attempt to fix GitHub actions failure

--- build.sh
+++ build.sh
@@ -5,10 +5,9 @@ run() { "$@" || die "command failed with rc=$?"; }
 
 BUILDER=ykasidit/android_ndk_c_rust_go_builder:latest
 
-build() { run docker run --rm -v $(pwd):/build "$BUILDER" /bin/sh -c "$*"; }
+build() { run docker run -u $(id -u) --rm -v $(pwd):/build "$BUILDER" /bin/sh -c "$*"; }
 
 err "Build script user: $(id)"
-
 build 'echo Docker user: $(id) && cd /build && ndk-build clean && ndk-build V=1 -j $(nproc)'
 
 err
