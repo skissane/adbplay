70c8d48ec53d008a135e12ba8ce046124a8d3c21
Simon Kissane <skissane@gmail.com>
2023-06-12 11:13:39 +1000

    debug Docker actions failure

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ BUILDER=ykasidit/android_ndk_c_rust_go_builder:latest
 
 build() { run docker run --rm -v $(pwd):/build "$BUILDER" /bin/sh -c "$*"; }
 
-build 'cd /build && ndk-build clean && ndk-build V=1 -j $(nproc)'
+err "Build script user: $(id)"
+
+build 'echo Docker user: $(id) && cd /build && ndk-build clean && ndk-build V=1 -j $(nproc)'
 
 err
 err "==== BUILD SUCCESSFUL ===="
